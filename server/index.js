95540b4ec2cee5756186f5de3240f3c098cb9dbb
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-06-15 10:50:10 +0200

    import mock data

--- server/index.js
+++ server/index.js
@@ -4,12 +4,20 @@ import cors from 'cors';
 import jwks from 'jwks-rsa';
 import bodyParser from 'body-parser';
 
+import data from './data.json';
+
 const app = express();
 const PORT = 4000;
 
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
+
+app.get('/courses', (req, res) => {
+    res.send({ data });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is runnig on port ${PORT}`);
