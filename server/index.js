9e6afdce918d390f68c40948da0e894b5d9aa390
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-06-16 22:00:15 +0200

    fix babel

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
+import { path } from 'path';
+
 import express from 'express';
 import jwt from 'express-jwt';
 import cors from 'cors';
 import jwks from 'jwks-rsa';
 import bodyParser from 'body-parser';
 
-import data from './data.json';
+//import data from './data.json';
 
 const app = express();
 const PORT = 4000;
@@ -15,9 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.get('/courses', (req, res) => {
-    res.send({ data });
-});
+// app.get('/courses', (req, res) => {
+//     res.send({ data });
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is runnig on port ${PORT}`);
